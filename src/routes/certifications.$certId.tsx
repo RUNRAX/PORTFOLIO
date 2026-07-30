1e36efb899fa88eb2cd2ef4fd3c6ad4442b2816e
Rakshit Awati <rakshitawati5@gmail.com>
2026-07-30 17:36:51 +0530

    Revert "Remove isolated stacking contexts on magnify-content and LiquidBackground"
    This reverts commit 228d7470f1c75179e5049bd5c4513a04fda9bef7.

--- src/routes/certifications.$certId.tsx
+++ src/routes/certifications.$certId.tsx
@@ -21,7 +21,7 @@ function CertDetail() {
     <main className="relative min-h-dvh text-foreground">
       <LiquidBackground />
       <Nav />
-      <div id="magnify-content" className="flex flex-col items-center min-h-dvh pt-32 pb-24 px-6">
+      <div id="magnify-content" className="relative z-10 min-h-dvh pt-32 pb-24 px-6 flex flex-col items-center">
         <div className="w-full max-w-4xl">
           <Link
             to="/"
